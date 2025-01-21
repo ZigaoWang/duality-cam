7bd4ee24b5e5b992c51be76d8a725bb94a15c892
Zigao Wang <zigao.wang24@gmail.com>
2025-01-21 15:08:14 +0800

    Initial Commit

---
+++ Duality Cam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Duality Cam
+//
+//  Created by Zigao Wang on 1/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Duality Cam/Duality_CamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Duality_CamApp.swift
+//  Duality Cam
+//
+//  Created by Zigao Wang on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Duality_CamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
